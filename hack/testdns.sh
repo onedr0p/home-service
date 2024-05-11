f13b1a2e66bc3c8f1099f5753efa3338197c6828
Devin Buhl <devin.kray@gmail.com>
2024-05-11 16:38:06 -0400

    cut over to dnsdist from host net to containernet

--- hack/testdns.sh
+++ hack/testdns.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
 echo "dnsdist external query"
-dig +short @192.168.1.42 google.com | sed 's/^/  /'
+dig +short @192.168.1.121 google.com | sed 's/^/  /'
 echo "bind external query"
 dig +short @192.168.1.123 google.com | sed 's/^/  /'
 echo "blocky external query"
 dig +short @192.168.1.122 google.com | sed 's/^/  /'
 echo "-----------------------------"
 echo "dnsdist internal query"
-dig +short @192.168.1.42 expanse.turbo.ac | sed 's/^/  /'
+dig +short @192.168.1.121 expanse.turbo.ac | sed 's/^/  /'
 echo "bind internal query"
 dig +short @192.168.1.123 expanse.turbo.ac | sed 's/^/  /'
 echo "blocky internal query"
 dig +short @192.168.1.122 expanse.turbo.ac | sed 's/^/  /'
 echo "-----------------------------"
 echo "dnsdist cluster query"
-dig +short @192.168.1.42 echo-server.devbu.io | sed 's/^/  /'
+dig +short @192.168.1.121 echo-server.devbu.io | sed 's/^/  /'
 echo "bind cluster query"
 dig +short @192.168.1.123 echo-server.devbu.io | sed 's/^/  /'
 echo "blocky cluster query"
