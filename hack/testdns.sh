8d5402e02d0eb9943e7f561e4a726393c94e8d90
Devin Buhl <devin.kray@gmail.com>
2024-05-16 16:20:16 -0400

    sync configs :duck:

--- hack/testdns.sh
+++ hack/testdns.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
-echo "blocky external query"
+echo "BLOCKY external query"
 dig +short @192.168.1.121 google.com | sed 's/^/  /'
-echo "bind external query"
+echo "BIND external query"
 dig +short @192.168.1.123 google.com | sed 's/^/  /'
-echo "-----------------------------"
-echo "blocky internal query"
-dig +short @192.168.1.121 expanse.internal | sed 's/^/  /'
-echo "bind internal query"
-dig +short @192.168.1.123 expanse.internal | sed 's/^/  /'
-echo "-----------------------------"
-echo "blocky cluster query"
+echo "---"
+echo "BLOCKY internal query"
+dig +short @192.168.1.121 unifi.internal | sed 's/^/  /'
+echo "BLOCKY internal BIND query"
+dig +short @192.168.1.121 unifi.turbo.ac | sed 's/^/  /'
+echo "---"
+echo "BLOCKY main cluster query"
 dig +short @192.168.1.121 echo-server.devbu.io | sed 's/^/  /'
-echo "bind cluster query"
+echo "BIND main cluster query"
 dig +short @192.168.1.123 echo-server.devbu.io | sed 's/^/  /'
-echo "-----------------------------"
-echo "blocky reverse query"
+echo "BIND storage cluster query"
+dig +short @192.168.1.123 s3.turbo.ac | sed 's/^/  /'
+echo "---"
+echo "BLOCKY reverse query"
 dig +short @192.168.1.121 -x 192.168.42.80 | sed 's/^/  /'
-echo "blocky reverse query"
-dig +short @192.168.1.123 -x 192.168.42.80 | sed 's/^/  /'
-echo "-----------------------------"
+echo "---"
