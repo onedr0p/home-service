85298ca130856a2ba8cc63c4604cadc5f2300dd9
Devin Buhl <devin.kray@gmail.com>
2024-05-16 10:20:27 -0400

    remove dnsdist and go full-on blocky

--- hack/testdns.sh
+++ hack/testdns.sh
@@ -1,23 +1,17 @@
 #!/usr/bin/env bash
 
-echo "dnsdist external query"
+echo "blocky external query"
 dig +short @192.168.1.121 google.com | sed 's/^/  /'
 echo "bind external query"
 dig +short @192.168.1.123 google.com | sed 's/^/  /'
-echo "blocky external query"
-dig +short @192.168.1.122 google.com | sed 's/^/  /'
 echo "-----------------------------"
-echo "dnsdist internal query"
+echo "blocky internal query"
 dig +short @192.168.1.121 expanse.turbo.ac | sed 's/^/  /'
 echo "bind internal query"
 dig +short @192.168.1.123 expanse.turbo.ac | sed 's/^/  /'
-echo "blocky internal query"
-dig +short @192.168.1.122 expanse.turbo.ac | sed 's/^/  /'
 echo "-----------------------------"
-echo "dnsdist cluster query"
+echo "blocky cluster query"
 dig +short @192.168.1.121 echo-server.devbu.io | sed 's/^/  /'
 echo "bind cluster query"
 dig +short @192.168.1.123 echo-server.devbu.io | sed 's/^/  /'
-echo "blocky cluster query"
-dig +short @192.168.1.122 echo-server.devbu.io | sed 's/^/  /'
 echo "-----------------------------"
