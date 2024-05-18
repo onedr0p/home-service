d18c0d2e48224754a5ec1ee42decc87338e79be2
Devin Buhl <devin.kray@gmail.com>
2024-05-17 21:37:34 -0400

    add more rice for secondary

--- hack/testdns.sh
+++ hack/testdns.sh
@@ -2,8 +2,6 @@
 
 echo "BLOCKY external query"
 dig +short @192.168.1.121 google.com | sed 's/^/  /'
-echo "BIND external query"
-dig +short @192.168.1.123 google.com | sed 's/^/  /'
 echo "---"
 echo "BLOCKY internal query"
 dig +short @192.168.1.121 unifi.internal | sed 's/^/  /'
@@ -19,4 +17,4 @@ dig +short @192.168.1.123 s3.turbo.ac | sed 's/^/  /'
 echo "---"
 echo "BLOCKY reverse query"
 dig +short @192.168.1.121 -x 192.168.42.80 | sed 's/^/  /'
-echo "---"
+echo "---------------------------------------------------"
